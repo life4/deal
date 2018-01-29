12317db625d16a39710f8112a95dc704e9439e42
Gram <master_fess@mail.ru>
2018-01-29 17:44:40 +0500

    +pre unit tests

--- tests.py
+++ tests.py
@@ -17,9 +17,58 @@ class PreTest(unittest.TestCase):
             with self.assertRaises(PreContractError):
                 func(-2)
 
+    def test_init(self):
+        with self.subTest(text='init doesn\'t raise any exceptions'):
+            func = pre(lambda x: x > 0)
+
+        with self.subTest(text='validator'):
+            func = pre(lambda x: x > 0)(lambda x: x)
+            with self.assertRaises(PreContractError):
+                func(-2)
+
+        with self.subTest(text='message'):
+            func = pre(lambda x: x > 0, message='TEST')(lambda x: x)
+            try:
+                func(-2)
+            except AssertionError as e:
+                self.assertEqual(e.args[0], 'TEST')
+
+        with self.subTest(text='exception'):
+            func = pre(lambda x: x > 0, exception=NameError)(lambda x: x)
+            with self.assertRaises(NameError):
+                func(-2)
+
+        with self.subTest(text='exception with name'):
+            func = pre(lambda x: x > 0, exception=NameError('TEST'))(lambda x: x)
+            with self.subTest(text='exception/exception'):
+                with self.assertRaises(NameError):
+                    func(-2)
+            with self.subTest(text='exception/message'):
+                try:
+                    func(-2)
+                except NameError as e:
+                    self.assertEqual(e.args[0], 'TEST')
+
+        with self.subTest(text='exception+message'):
+            func = pre(lambda x: x > 0, message='TEST', exception=NameError)(lambda x: x)
+            with self.subTest(text='exception+message/exception'):
+                with self.assertRaises(NameError):
+                    func(-2)
+            with self.subTest(text='exception+message/message'):
+                try:
+                    func(-2)
+                except NameError as e:
+                    self.assertEqual(e.args[0], 'TEST')
+
+    def test_call(self):
+        with self.subTest(text='call return patched function'):
+            deco = pre(lambda x: x > 0)
+            func = deco(lambda x: x)
+            self.assertIsInstance(func, type(deco.patched_function))
+
 
 class PostTest(unittest.TestCase):
-    def test_true_false(self):
+    def test_main(self):
         func = post(lambda x: x > 0)(lambda x: x)
         with self.subTest(text='good'):
             self.assertEqual(func(4), 4)
@@ -29,7 +78,7 @@ class PostTest(unittest.TestCase):
 
 
 class InvTest(unittest.TestCase):
-    def test_true_false(self):
+    def test_main(self):
         @inv(lambda obj: obj.x > 0)
         class A(object):
             x = 2
