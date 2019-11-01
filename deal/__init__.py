bd9deba02a0ef8ddf729840971690dc9af1419f6
Gram <master_fess@mail.ru>
2019-11-01 14:25:04 +0100

    release

--- deal/__init__.py
+++ deal/__init__.py
@@ -15,7 +15,7 @@ This library contain 3 main conception from DbC:
 
 # main package info
 __title__ = 'deal'
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 __author__ = 'Gram Orsinium'
 __license__ = 'MIT'
 
