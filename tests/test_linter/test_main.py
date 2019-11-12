94026742293f9173eca315ee031f5660be6d3e62
Gram <master_fess@mail.ru>
2019-11-12 11:32:31 +0100

    fix test on CI

--- tests/test_linter/test_main.py
+++ tests/test_linter/test_main.py
@@ -3,5 +3,5 @@ import sys
 
 
 def test_cli_calling():
-    result = subprocess.run([sys.executable, '-m', 'deal.linter', 'setup.py'])
+    result = subprocess.run([sys.executable, '-m', 'deal.linter', __file__])
     assert result.returncode == 0
