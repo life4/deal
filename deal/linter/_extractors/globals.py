ff68a2abd85b7b7116f6a9cc848736bc6ab17bc4
Gram <master_fess@mail.ru>
2020-04-24 10:42:08 +0200

    point on keyword instead of var in globals check

--- deal/linter/_extractors/globals.py
+++ deal/linter/_extractors/globals.py
@@ -12,24 +12,25 @@ from .common import TOKENS, Token, traverse
 def get_globals(body: list) -> Iterator[Token]:
     for expr in traverse(body):
         if isinstance(expr, TOKENS.GLOBAL):
-            for name in expr.names:
-                yield Token(value=name, line=expr.lineno, col=expr.col_offset + 7)
+            yield Token(value='global', line=expr.lineno, col=expr.col_offset)
+            continue
+
+        if isinstance(expr, TOKENS.NONLOCAL):
+            yield Token(value='nonlocal', line=expr.lineno, col=expr.col_offset)
             continue
 
         if type(expr) is ast.Import:
-            for alias in expr.names:
-                yield Token(value=alias.name, line=expr.lineno, col=expr.col_offset + 7)
+            yield Token(value='import', line=expr.lineno, col=expr.col_offset)
             continue
 
         if type(expr) is astroid.Import:
-            for name, _ in expr.names:
-                yield Token(value=name, line=expr.lineno, col=expr.col_offset + 7)
+            yield Token(value='import', line=expr.lineno, col=expr.col_offset)
             continue
 
         if type(expr) is ast.ImportFrom:
-            yield Token(value=expr.module, line=expr.lineno, col=expr.col_offset + 5)
+            yield Token(value='import', line=expr.lineno, col=expr.col_offset)
             continue
 
         if type(expr) is astroid.ImportFrom:
-            yield Token(value=expr.modname, line=expr.lineno, col=expr.col_offset + 5)
+            yield Token(value='import', line=expr.lineno, col=expr.col_offset)
             continue
--- tests/test_linter/test_extractors/test_globals.py
+++ tests/test_linter/test_extractors/test_globals.py
@@ -10,18 +10,18 @@ from deal.linter._extractors import get_globals
 
 
 @pytest.mark.parametrize('text, expected', [
-    ('global a', ('a', )),
-    ('global a, b, c', ('a', 'b', 'c')),
+    ('global a', ('global', )),
+    ('global a, b, c', ('global', )),
 
-    ('nonlocal a', ('a', )),
-    ('nonlocal a, b, c', ('a', 'b', 'c')),
+    ('nonlocal a', ('nonlocal', )),
+    ('nonlocal a, b, c', ('nonlocal', )),
 
-    ('import a', ('a', )),
-    ('import a as b', ('a', )),
-    ('import a as b, c', ('a', 'c')),
+    ('import a', ('import', )),
+    ('import a as b', ('import', )),
+    ('import a as b, c', ('import', )),
 
-    ('from a import b', ('a', )),
-    ('from a import b as c', ('a', )),
+    ('from a import b', ('import', )),
+    ('from a import b as c', ('import', )),
 ])
 def test_get_globals_simple(text, expected):
     tree = astroid.parse(text)
