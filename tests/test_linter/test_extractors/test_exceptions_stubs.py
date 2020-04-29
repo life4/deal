a7750bf4dbd31702876810e8b288b7abbc639419
Gram <master_fess@mail.ru>
2020-04-29 08:53:42 +0200

    test stubs export from the module location

--- tests/test_linter/test_extractors/test_exceptions_stubs.py
+++ tests/test_linter/test_extractors/test_exceptions_stubs.py
@@ -1,5 +1,6 @@
 # built-in
 import json
+import sys
 from textwrap import dedent
 
 # external
@@ -11,7 +12,7 @@ from deal.linter._extractors.exceptions_stubs import _get_full_name
 from deal.linter._stub import StubsManager
 
 
-def test_from_stubs(tmp_path):
+def test_stubs_in_the_root(tmp_path):
     root = tmp_path / 'project'
     root.mkdir()
     (root / '__init__.py').touch()
@@ -21,7 +22,6 @@ def test_from_stubs(tmp_path):
     stubs = StubsManager(paths=[root])
 
     text = """
-        # from project.other import parent
         from math import isnan
 
         @deal.raises()
@@ -34,6 +34,33 @@ def test_from_stubs(tmp_path):
     func_tree = tree.body[-1].body
     returns = tuple(r.value for r in get_exceptions_stubs(body=func_tree, stubs=stubs))
     assert returns == (ZeroDivisionError, )
+
+
+def test_stubs_next_to_imported_module(tmp_path):
+    root = tmp_path / 'project'
+    root.mkdir()
+    (root / '__init__.py').touch()
+    (root / 'other.py').write_text('def parent(): pass')
+    stub = {'parent': {'raises': ['ZeroDivisionError']}}
+    (root / 'other.json').write_text(json.dumps(stub))
+    stubs = StubsManager()
+
+    text = """
+        from project.other import parent
+
+        @deal.raises()
+        def child():
+            parent()
+    """
+    sys.path.append(str(tmp_path))
+    try:
+        tree = astroid.parse(dedent(text))
+        print(tree.repr_tree())
+        func_tree = tree.body[-1].body
+        returns = tuple(r.value for r in get_exceptions_stubs(body=func_tree, stubs=stubs))
+        assert returns == (ZeroDivisionError, )
+    finally:
+        sys.path = sys.path[:-1]
 
 
 def test_get_full_name_func():
