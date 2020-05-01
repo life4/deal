7f33cbc595ed31519cefdfaaf6f415dada5acd94
Gram <master_fess@mail.ru>
2020-05-01 14:30:56 +0200

    migrate on sphinx 3.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,9 @@ from datetime import date
 from pathlib import Path
 
 # external
+import sphinx
 import sphinx_rtd_theme
+from m2r import MdInclude
 from recommonmark.transform import AutoStructify
 
 
@@ -18,7 +20,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'm2r',
+    'recommonmark',
 ]
 
 templates_path = ['_templates']
@@ -93,3 +95,10 @@ def setup(app):
     }
     app.add_config_value('recommonmark_config', config, True)
     app.add_transform(AutoStructify)
+
+    # from m2r to make `mdinclude` work
+    app.add_config_value('no_underscore_emphasis', False, 'env')
+    app.add_config_value('m2r_parse_relative_links', False, 'env')
+    app.add_config_value('m2r_anonymous_references', False, 'env')
+    app.add_config_value('m2r_disable_inline_math', False, 'env')
+    app.add_directive('mdinclude', MdInclude)
