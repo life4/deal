637f81a3fd2869fa83a4e11966fc5aee511dbd2e
Gram <master_fess@mail.ru>
2020-05-11 10:38:17 +0200

    cover all

--- tests/test_cli/test_test.py
+++ tests/test_cli/test_test.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from textwrap import dedent
 
 # project
-from deal._cli._test import test_command as command
+from deal._cli._test import test_command as command, sys_path
 
 
 def test_safe_violation(tmp_path: Path, capsys):
@@ -62,3 +62,42 @@ def test_no_violations(tmp_path: Path):
     assert 'running func' in captured
     assert 'not_pure' not in captured
     assert 'func(' not in captured
+
+
+def test_no_matching_funcs(tmp_path: Path):
+    if 'example' in sys.modules:
+        del sys.modules['example']
+    text = """
+        import deal
+
+        def not_pure1(a: int, b: int) -> float:
+            return a / b
+
+        @deal.post(lambda result: result > 0)
+        def not_pure2(a: int, b: int) -> float:
+            return a / b
+    """
+    path = (tmp_path / 'example.py')
+    path.write_text(dedent(text))
+    stream = StringIO()
+    result = command(['--count', '5', str(path)], root=tmp_path, stream=stream)
+    assert result == 0
+
+    stream.seek(0)
+    captured = stream.read()
+    assert '/example.py' not in captured
+
+
+def test_sys_path():
+    path = Path('example')
+    size = len(sys.path)
+
+    assert sys.path[0] != 'example'
+    with sys_path(path):
+        assert sys.path[0] == 'example'
+    assert sys.path[0] != 'example'
+    assert len(sys.path) == size
+
+    with sys_path(path):
+        del sys.path[0]
+    assert len(sys.path) == size
