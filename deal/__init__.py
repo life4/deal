ba81509c102c4d588531964a8bd093443d9e665c
Gram <master_fess@mail.ru>
2020-09-11 14:59:29 +0200

    bump version to 4.0.1

--- deal/__init__.py
+++ deal/__init__.py
@@ -8,7 +8,7 @@ See [documentation][docs] for more details.
 
 # main package info
 __title__ = 'deal'
-__version__ = '4.0.0'
+__version__ = '4.0.1'
 __author__ = 'Gram Orsinium'
 __license__ = 'MIT'
 
