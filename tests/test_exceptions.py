912ea5687fb2dc1e5a2465b9598a630806828979
Gram <master_fess@mail.ru>
2020-10-08 10:16:35 +0200

    test excepthook

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,5 +1,10 @@
+import sys
+from pathlib import Path
+
 import deal
 import pytest
+from deal._decorators.base import Base
+from deal._exceptions import exception_hook, _excepthook
 
 
 def test_source_get_lambda_from_dec():
@@ -78,3 +83,37 @@ def test_source_get_chain():
     with pytest.raises(deal.ContractError) as exc_info:
         sum(2, -3)
     assert exc_info.value.source == 'b > 0'
+
+
+def test_exception_hook(capsys):
+    pre_path = str(Path('deal', '_decorators', 'pre.py'))
+    f = deal.pre(lambda x: x > 0)(lambda x: x)
+    with pytest.raises(deal.PreContractError) as exc_info:
+        f(-2)
+
+    # custom hook is registered
+    assert sys.excepthook is exception_hook
+
+    # the default hook shows the full traceback
+    _excepthook(exc_info.type, exc_info.value, exc_info.tb)
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    assert pre_path in captured.err
+
+    # the custom hook hides deal from the traceback
+    exception_hook(exc_info.type, exc_info.value, exc_info.tb)
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    assert pre_path not in captured.err
+
+
+def test_exception_hook_ignores_non_contract_exceptions(capsys):
+    with pytest.raises(NotImplementedError) as exc_info:
+        Base.patched_function(None)
+
+    # the custom hook does not reduce non-contract tracebacks
+    exception_hook(exc_info.type, exc_info.value, exc_info.tb)
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    base_path = str(Path('deal', '_decorators', 'base.py'))
+    assert base_path in captured.err
