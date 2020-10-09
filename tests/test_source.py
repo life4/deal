0b64fc650f3d16e8d59a5cb73ce996d901bca415
Gram <master_fess@mail.ru>
2020-10-09 15:43:22 +0200

    test a bit more

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,22 +1,22 @@
+# autopep8: off
 from deal._source import get_validator_source
 
 
 def test_get_validator_source():
-    # autopep8: off
-    c = lambda x: x > 0 # noqa
-    # autopep8: on
-
+    c = lambda x: x > 0  # noqa
     assert get_validator_source(c) == 'x > 0'
 
 
 def test_get_validator_source_multiline_lambda():
-    # autopep8: off
     c = (lambda
-        x: x > 0    # noqa
-        )           # noqa
-    # autopep8: on
-
+        x: x > 0    # noqa: E128
+        )           # noqa: E124
     assert get_validator_source(c) == 'x > 0'
+
+
+def test_get_validator_source_multiple_braces():
+    c = ((lambda x: x > 0))
+    assert 'x > 0' in get_validator_source(c)
 
 
 def test_get_validator_source_no_source():
