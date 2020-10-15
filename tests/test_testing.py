cc87d5dae55ff5528ea86c8183b5e83bb6082361
orsinium <master_fess@mail.ru>
2020-10-15 11:03:17 +0200

    actualize hypothesis tests

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -10,9 +10,15 @@ import deal
 
 
 @deal.raises(ZeroDivisionError)
+def div1(a: int, b: int) -> float:
+    assert isinstance(a, int)
+    assert isinstance(b, int)
+    return a / b
+
+
 @deal.pre(lambda a, b: a > 0)
 @deal.pre(lambda a, b: b > 0, message='b must be positive')
-def div(a: int, b: int) -> float:
+def div2(a: int, b: int) -> float:
     assert isinstance(a, int)
     assert isinstance(b, int)
     assert a > 0
@@ -20,32 +26,46 @@ def div(a: int, b: int) -> float:
     return a / b
 
 
-func = div
-
-
 def test_count():
     for count in (1, 10, 20, 50):
-        cases = deal.cases(func, count=count)
+        cases = deal.cases(div1, count=count)
+        assert len(list(cases)) == count
+
+        cases = deal.cases(div2, count=count)
         assert len(list(cases)) == count
 
 
 def test_params_detected():
-    for case in deal.cases(func, count=10):
+    for case in deal.cases(div1, count=10):
+        assert set(case.kwargs) == {'a', 'b'}
+
+    for case in deal.cases(div2, count=10):
         assert set(case.kwargs) == {'a', 'b'}
 
 
 def test_params_type():
-    for case in deal.cases(func, count=10):
+    for case in deal.cases(div1, count=10):
+        assert type(case.kwargs['a']) is int
+        assert type(case.kwargs['b']) is int
+
+    for case in deal.cases(div2, count=10):
         assert type(case.kwargs['a']) is int
         assert type(case.kwargs['b']) is int
 
 
 def test_params_ok_with_excs():
     results = []
-    for case in deal.cases(func, count=20):
+    for case in deal.cases(div1, count=20):
         result = case()
         results.append(result)
+    assert len(results) == 20
     assert any(r is NoReturn for r in results), 'no exception occured'
+
+
+def test_no_bad_examples():
+    for case in deal.cases(div2, count=20):
+        assert case.kwargs['a'] > 0
+        assert case.kwargs['b'] > 0
 
 
 def test_return_type_checks():
