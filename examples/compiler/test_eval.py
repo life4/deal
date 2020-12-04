ffa4917866cea0a53fed1063fd764967bb8fd608
gram <mail@orsinium.dev>
2020-12-04 17:39:16 +0100

    property test eval

--- examples/compiler/test_eval.py
+++ examples/compiler/test_eval.py
@@ -1,3 +1,4 @@
+import deal
 from ._eval import eval, IntNode, AddNode
 
 
@@ -10,3 +11,18 @@ def test_eval():
     node = AddNode(IntNode(3), IntNode(4))
     node = AddNode(IntNode(2), node)
     assert eval(node) == 2 + 3 + 4
+
+
+def test_eval_prop():
+    import hypothesis.strategies as st
+
+    strategy = st.one_of(
+        st.builds(IntNode, st.integers()),
+        st.deferred(lambda: st.builds(AddNode, strategy, strategy)),
+    )
+    cases = deal.cases(
+        func=eval,
+        kwargs=dict(node=strategy),
+    )
+    for case in cases:
+        case()
