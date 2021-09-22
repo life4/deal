502f6a06cd588267777d475300fdcb74cc004bba
gram <mail@orsinium.dev>
2021-09-22 15:50:35 +0200

    usage example for introspection

--- deal/introspection/__init__.py
+++ deal/introspection/__init__.py
@@ -2,6 +2,24 @@
 The module provides `get_contracts` function which enumerates
 contracts wrapping the given function. Every contract is returned
 in wrapper providing a stable interface.
+
+Usage example:
+
+```python
+import deal
+
+@deal.pre(lambda x: x > 0)
+def f(x):
+    return x + 1
+
+contracts = deal.introspection.get_contracts(f)
+for contract in contracts:
+    assert isinstance(contract, deal.introspection.Contract)
+    assert isinstance(contract, deal.introspection.Pre)
+    assert contract.source == 'x > 0'
+    assert contract.exception is deal.PreContractError
+    contract.validate(1)
+```
 """
 
 from ._extractor import get_contracts
